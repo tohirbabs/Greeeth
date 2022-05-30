cc2a3a4a2e7b11a16fc402a75474b032d64401d5
chuongtang <chuong.tang@aol.com>
2022-05-30 14:55:51 -0600

    added how it work and responsive design

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Home from './components/Home';
 import Solution from './components/Solution';
+import HowItWorks from './components/HowItWorks';
 
 function App() {
   return (
     <div className="App">
       <Home />
       <Solution />
+      <HowItWorks />
     </div>
   );
 }
