30e25fb6f592de21abce285c52a247076d39515c
chuongtang <chuong.tang@aol.com>
2022-06-20 15:32:15 -0600

    Added Props for resuable

--- src/components/DetectLocation.jsx
+++ src/components/DetectLocation.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import PinsOnGlobe from '../../assets/PinsOnGlobe.png';
+import BG1 from '../../assets/BG1.jpg';
 
 function DetectLocation() {
   return (
-    <div className="py-16 bg-white BG1 py-42">
+    <div className="py-16 bg-white BG1 py-42" style={{ backgroundImage: `url(${BG1})` }}>
       <div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
         <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
           <div className="md:5/12 lg:w-5/12">
--- src/components/PixLeftTextRight.jsx
+++ src/components/PixLeftTextRight.jsx
@@ -1,23 +1,17 @@
 import React from 'react';
-import PinsOnGlobe from '../../assets/PinsOnGlobe.png';
 
-function PixLeftTextRight({BG, Pix, TextList}) {
+function PixLeftTextRight({ BG, Pix, TextList }) {
   return (
-    <div className="py-16 bg-white BG1 py-42">
+    <div className="py-16 bg-white BG1 py-42" style={{ backgroundImage: `url(${BG})` }}>
       <div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
         <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
           <div className="md:5/12 lg:w-5/12">
-            <img src={PinsOnGlobe} alt="Location pins illustration" loading="lazy" className="mx-auto" />
+            <img src={Pix} alt="Location pins illustration" loading="lazy" className="mx-auto" />
           </div>
           <div className="text-left md:7/12 lg:w-6/12 ml-12">
             <h2 className="text-2xl text-gray-900 font-bold md:text-4xl">Detect planting location</h2>
             <ul className="mt-6 text-gray-600 list-disc leading-loose">
-              <li>Get data of vulnurable location</li>
-              <li>We check for plantable area</li>
-              <li>Individuals/communities/locals csn map out planting locations on the map</li>
-              <li>Check location with map, more CO2 emission on the map</li>
-              <li>Choose where to plant (manually)</li>
-              <li>Vet foundation with location</li>
+              {TextList}
             </ul>
           </div>
         </div>
@@ -26,4 +20,4 @@ function PixLeftTextRight({BG, Pix, TextList}) {
   );
 }
 
-export default PixLeftTextRight;{BG, Pix, TextList}
+export default PixLeftTextRight;
