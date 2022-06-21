6adf00373852140c98910179bd24cff30e74314d
chuongtang <chuong.tang@aol.com>
2022-06-20 22:16:43 -0600

    added extracted assets for Process

--- src/screens/Process.jsx
+++ src/screens/Process.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 import { UnderstandProcess, DetectLocation, SendLocation } from '../components';
+import PixLeftTextRight from '../components/PixLeftTextRight';
 
 function Process() {
+  //   const renderOrder = (arr) => {
+  //     arr.forEach((item, i) => {
+  //       if ((i + 1) % 2 === 0) {
+  //         return <PixLeftTextRight ({ BG = item.BG }, { Pix = item.Pix }, { TextList = item.TextList }
+  //       ) />
+  //   } else {
+  //       return (<PixRightTextLeft ({{ BG=item.BG }, { Pix=item.Pix }, { TextList =item.TextList }}) />)
+  // }
+  //     })
+  //   };
   return (
     <div>
       <UnderstandProcess />
