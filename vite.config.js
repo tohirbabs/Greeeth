743d586bba9ad55ec34516b697bfc9d6e33119ad
dev_panda <75636672+TohirBabs@users.noreply.github.com>
2022-07-01 13:26:47 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 // eslint-disable-next-line import/no-extraneous-dependencies
-import react from '@vitejs/plugin-react';
+import react from "@vitejs/plugin-react";
 // eslint-disable-next-line import/no-extraneous-dependencies
-import WindiCSS from 'vite-plugin-windicss';
+import WindiCSS from "vite-plugin-windicss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
