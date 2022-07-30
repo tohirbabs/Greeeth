1aa75dc27ef49b3524c0b21a596cd33e5aa9b469
dev_panda <75636672+TohirBabs@users.noreply.github.com>
2022-07-30 17:44:42 +0100

    updates

--- src/screens/Process.jsx
+++ src/screens/Process.jsx
@@ -1,27 +1,11 @@
 import React from "react";
-import {
-  ProcessHome,
-  Solution,
-  HowItWorks,
-  PlantByHuman,
-  PlantWithDrone,
-  ManageEmission,
-  Footer,
-  ProcessSection,
-} from "../components";
+import { ProcessHome, ProcessSection } from "../components";
 
 function Process() {
   return (
     <div>
       <ProcessHome />
       <ProcessSection />
-      {/* <ProcessSection /> */}
-      {/* <Solution />
-      <HowItWorks />
-      <PlantByHuman />
-      <PlantWithDrone />
-      <ManageEmission /> */}
-      <Footer />
     </div>
   );
 }
