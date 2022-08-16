2302c5c94cdac75f728076b7b988eaff80e7b293
dev_panda <75636672+TohirBabs@users.noreply.github.com>
2022-08-16 01:50:28 +0100

    update

--- src/components/FootprintSection.jsx
+++ src/components/FootprintSection.jsx
@@ -803,15 +803,38 @@ function FootprintSection() {
 
   const Cloth = () => {
     return (
-      <>
-        {wasteArray.map((comp, i) => (
-          <CompaniesList
-            Pix={comp.Illustration}
-            TextList={comp.TextList}
-            Index={i}
-          />
-        ))}
-      </>
+      <div className="py-5  flex  flex-col  flex-wrapitems-center justify-center BG1">
+        <div className="container flex items-center max-w-screen-xl m-auto py-5 md:py-10 text-gray-600  md:px-12 xl:px-22  BG-lgreen w-screen">
+          <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
+            <div className="md:5/12 lg:w-6/12 w-9/10 mx-auto">
+              <img
+                src={Pix4}
+                alt="Location pins illustration"
+                loading="lazy"
+                className="mx-auto"
+              />
+            </div>
+            <div className="text-center md:6/12 lg:w-6/12 md:ml-12 mx-3">
+              <h2 className="text-2xl lgreen font-bold md:text-4xl">
+                Clothing and Footwear
+              </h2>
+              <div className="mt-6 text-gray-600 leading-loose">
+                Calculate upstream emissions produced by entering average amount
+                spent on purchasing clothes and footwear (in euros)
+              </div>
+              <div className="flex flex-wrap gap-4 mt-10 text-center mx-auto w-1/2">
+                <input
+                  type="number"
+                  id="state"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 pl-4 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="0"
+                  required
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   };
 
