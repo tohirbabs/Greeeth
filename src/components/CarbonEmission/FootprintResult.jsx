1dd2fcbe9aa520d909fd0f043bc576aa83c76b4d
dev_panda <75636672+TohirBabs@users.noreply.github.com>
2022-08-24 13:18:25 +0100

    update

--- src/components/CarbonEmission/FootprintResult.jsx
+++ src/components/CarbonEmission/FootprintResult.jsx
@@ -3,12 +3,12 @@ import { global, percent } from "../../../assets/CarbonEmissions";
 
 export const FootprintResult = () => {
   return (
-    <div className="sm:flex-row flex flex-col gap-10 my-10">
-      <div className="footprint   sm:w-1/2 bg-lgreen rounded-xl p-8 px-2 sm:px-8 text-white flex flex-col items-center">
+    <div className="sm:flex-row flex flex-col gap-10 my-10 sm:pb-20">
+      <div className="footprint   sm:w-1/2 bg-white rounded-xl p-8 px-2 sm:px-8 ligreen flex flex-col items-center sm:pb-20">
         <h3 className="text-3xl font-bold w-full sm:text-left">
           CONGRATULATIONS!
         </h3>
-        <p className="w-full text-lg mt-6 sm:text-left">
+        <p className="w-full text-lg mt-6 sm:text-left text-gray">
           Your annual footprint is well below the Global average. Keep up the
           great work and share your score!
         </p>
@@ -16,13 +16,13 @@ export const FootprintResult = () => {
           <p className="sm:text-2xl text-xl">YOUR FOOTPRINT IS EQUAL TO</p>
           <p className="text-7xl">7.4</p>
           <p className="text-5xl">TONNES*</p>
-          <div className=" rounded-xl p-4 w-full border-2 bd-lgreen relative w-9/10 p-3 text-base  my-3 cursor-pointer  bg-white lgreen">
+          <div className=" rounded-xl p-4 w-full relative w-9/10 p-3 text-base  my-3 cursor-pointer  bg-lgreen text-white">
             SHARE SCORE
           </div>
         </div>
       </div>
-      <div className="averages font-bold text-left sm:w-1/2 flex flex-col  gap-8 ligreen">
-        <div className="year bg-white sm:h-1/2 rounded-xl p-4 px-4 flex items-center gap-4">
+      <div className="averages font-bold text-left sm:w-1/2 flex flex-col  gap-8 text-white">
+        <div className="year bg-lgreen sm:h-1/2 rounded-xl p-4 px-4 flex items-center gap-4">
           <img className="max-w-1/3" src={global} alt="" />
           <div className="">
             <h3 className="text-lg">WORLD AVERAGE FOOTPRINT FOR 2022</h3>
@@ -30,7 +30,7 @@ export const FootprintResult = () => {
             <p className="text-3xl">TONNES</p>
           </div>
         </div>
-        <div className="percent sm:h-1/2 bg-white rounded-xl p-4 px-4 flex items-center gap-4">
+        <div className="percent sm:h-1/2 bg-lgreen rounded-xl p-4 px-4 flex items-center gap-4">
           <img className="max-w-1/3 max-h-30" src={percent} alt="" />
           <div className="">
             <h3 className="sm:text-xl text-lg">YOUR FOOTPRINT IS</h3>
