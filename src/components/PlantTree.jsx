63ea9c801ee8822906bd1cdb9f597a069086cffa
dev_panda <75636672+TohirBabs@users.noreply.github.com>
2022-08-26 15:24:33 +0100

    fixwe

--- src/components/PlantTree.jsx
+++ src/components/PlantTree.jsx
@@ -8,7 +8,7 @@ import confirm from "../../assets/landing/confirm.png";
 export const PlantTree = () => {
   const Card = ({ img, text, tag }) => {
     return (
-      <div className="p-4 bg-white rounded-xl relative text-center flex flex-col items-center max-w-150px">
+      <div className="sm:p-4 p-2 bg-white rounded-xl relative text-center w-4/12 flex flex-col items-center max-w-150px">
         <p className="text-xl font-bold ligreen absolute left-2 top-1">
           {tag}.
         </p>
@@ -19,12 +19,12 @@ export const PlantTree = () => {
   };
   return (
     <div className="my-8 sm:w-8/10">
-      <div className="flex justify-between my-10">
+      <div className="flex justify-between my-10 gap-2">
         <Card tag="1" img={locate} text="Detect planting location" />
         <Card tag="2" img={send} text="User plants trees" />
-        <Card tag="3" img={locate} text="User getrewarded" />
+        <Card tag="3" img={locate} text="User plants trees" />
       </div>
-      <div className="flex justify-between mt-15">
+      <div className="flex justify-between mt-15 gap-4">
         <Card tag="4" img={confirm} text="Confirmation" />
         <Card tag="5" img={cloud} text="Connect to the cloud and map" />
         <Card tag="6" img={rewardicon} text="User get rewarded" />
