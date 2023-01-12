8555fdd951eeaf7dbc87626a53e1c9b7a2f93c3c
dev_panda <75636672+TohirBabs@users.noreply.github.com>
2023-01-12 14:36:56 +0100

    upload waste modal

--- src/components/CarbonEmission/UploadWasteInfo.jsx
+++ src/components/CarbonEmission/UploadWasteInfo.jsx
@@ -50,8 +50,6 @@ export const UploadWasteInfo = () => {
     left: "50%",
     transform: "translate(-50%, -50%)",
     width: 400,
-    bgcolor: "background.paper",
-    border: "2px solid #000",
     boxShadow: 24,
     p: 4,
     background: "#daffda",
