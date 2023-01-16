10ae8722ff36f65caf109a6d27674cf15a60871e
dev_panda <75636672+TohirBabs@users.noreply.github.com>
2023-01-16 17:04:36 +0100

    waste

--- src/screens/WasteShop.jsx
+++ src/screens/WasteShop.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { easter } from "../../assets/GetInvolved/special_occasions/index";
+import partners from "../../assets/waste.png";
 import waste from "../../assets/gall.png";
 export const WasteShop = () => {
   const WasteCard = ({ wasteType, cost }) => {
@@ -26,12 +26,41 @@ export const WasteShop = () => {
   };
 
   return (
-    <div className="m-8 flex flex-wrap gap-10 justify-center ">
-      <WasteCard wasteType="PLASTIC" cost="5.87" />
-      <WasteCard wasteType="GLASS" cost="15.83" />
-      <WasteCard wasteType="WOOD" cost="52.81" />
-      <WasteCard wasteType="PLASTIC" cost="2.87" />
-      <WasteCard wasteType="GLASS" cost="5.85" />
+    <div>
+      <section className=" grad-lgreen flex-wrap items-center justify-center h-viewheight flex flex-col">
+        <div className="container h-full  lg:mx-6 my-3 flex flex-col flex-wrap items-center justify-around lg:max-w-screen-xl px-2 py-2 lg:ml-12 lg:h-full  lg:items-center lg:flex text-zinc-800">
+          <div className="lg:max-w-6/11 text-center darkgreen">
+            <h1 className="text-6xl  sm:(text-7xl !leading-tight) font-medium capitalize lgreen">
+              Waste Recycling
+            </h1>
+
+            <p className="mt-6 sm:leading-relaxed sm:text-xl">
+              Greeth makes it easier for individuals to sell their waste
+              (plastic, gless, metal, paper) for it to be recycled, when they
+              are calculating their carbon footprint
+            </p>
+            <div className="gap-4 mt-10 mx-auto text-center w-1/2">
+              <a
+                className="block py-4 text-base font-normal bg-lgreen rounded-lg shadow-md text-white sm:w-auto hover:text-white hover:bg-green-900 hover:border-white hover:border-2 active:text-rose-500 focus:outline-none focus:ring animate-bounce"
+                href="/about"
+              >
+                Sell Recyclable Waste
+              </a>
+            </div>
+          </div>
+          <img
+            src={partners}
+            alt="Tree illustration"
+            className="mx-auto lg:max-w-7/11"
+          />
+        </div>
+      </section>
+      <div className="m-8 flex flex-wrap gap-10 justify-center ">
+        <WasteCard wasteType="GLASS" cost="15.83" />
+        <WasteCard wasteType="WOOD" cost="52.81" />
+        <WasteCard wasteType="PLASTIC" cost="2.87" />
+        <WasteCard wasteType="GLASS" cost="5.85" />
+      </div>
     </div>
   );
 };
