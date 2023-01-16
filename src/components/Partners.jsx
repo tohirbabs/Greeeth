75b5d56eca8d7c0d774ac828b7a861312e28570c
dev_panda <75636672+TohirBabs@users.noreply.github.com>
2023-01-16 17:03:50 +0100

    partners

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -8,7 +8,7 @@ export const Partners = () => {
     <div className="py-20 flex flex-col items-center justify-center  px-2  lg:items-center lg:flex text-zinc-800">
       <div className="text-left darkgreen">
         <h1 className="text-3xl text-center sm:(text-5xl !leading-tight) font-medium capitalize text-zinc-800">
-          Partners
+          Milestones
         </h1>
         <section class="bg-white p-20 lg:p-[120px]">
           <div class="container mx-auto">
@@ -17,21 +17,30 @@ export const Partners = () => {
                 <div class="flex flex-wrap items-center justify-center">
                   <a
                     href="javascript:void(0)"
-                    class="mx-4 flex w-[150px] items-center justify-center py-5 2xl:w-[180px]"
+                    class="mx-4 flex flex-col gap-4 w-[150px] items-center justify-center py-5 2xl:w-[180px]"
                   >
-                    <img src={part1} alt="image" class="min-h-15 w-full" />
+                    <img src={part1} alt="image" class="h-30 w-30 w-full" />
+                    <p className="text-center">
+                      Ericsson Africa EIA winner 2022
+                    </p>
                   </a>
                   <a
                     href="javascript:void(0)"
-                    class="mx-4 flex w-[150px] items-center justify-center py-5 2xl:w-[180px]"
+                    class="mx-4 flex flex-col gap-4 w-[150px] items-center justify-center py-5 2xl:w-[180px]"
                   >
-                    <img src={part2} alt="image" class="min-h-15 w-full" />
+                    <img src={part2} alt="image" class="h-30 w-30 w-full" />
+                    <p className="text-center">
+                      Ericsson Africa EIA winner 2022
+                    </p>
                   </a>
                   <a
                     href="javascript:void(0)"
-                    class="mx-4 flex w-[150px] items-center justify-center py-5 2xl:w-[180px]"
+                    class="mx-4 flex flex-col gap-4 w-[150px] items-center justify-center py-5 2xl:w-[180px]"
                   >
-                    <img src={ibm} alt="image" class="min-h-15 w-full" />
+                    <img src={ibm} alt="image" class="h-30 py-6 w-30 w-full" />
+                    <p className="text-center">
+                      IBM callforcode protecting the environment winner 2022
+                    </p>
                   </a>
                 </div>
               </div>
