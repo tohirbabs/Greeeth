3c9ea1dade4ec54e5b064194bcbe642a127dac45
dev_panda <75636672+TohirBabs@users.noreply.github.com>
2023-01-16 17:04:05 +0100

    spinner

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,21 @@
+import '../App.css'
+const Spinner = () => {
+    return ( 
+        <div className="w-full flex h-48 justify-center items-center">
+            <div class="spinner">
+  <div></div>   
+  <div></div>    
+  <div></div>    
+  <div></div>    
+  <div></div>    
+  <div></div>    
+  <div></div>    
+  <div></div>    
+  <div></div>    
+  <div></div>    
+</div>
+        </div>
+     );
+}
+ 
+export default Spinner;
